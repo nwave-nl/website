23d9233a149ac1a86ecacc3b4411ac8c62c2b2fd
Marco Bruijns <40139212+marcobruijns@users.noreply.github.com>
2019-11-14 19:34:23 +0100

    Wat kunt u verwachten update

--- private/views/standard_layout.php
+++ private/views/standard_layout.php
@@ -5,13 +5,31 @@
   <link rel="stylesheet" href="<?php echo css('home'); ?>">
   <link rel="stylesheet" href="public/css/main.css">
   <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
-  <script src="public/js/nav.js"></script>
+  <script src="<?php echo js("nav"); ?>"></script>
 </head>
 <body>
+
+<div class="nav">
+    <div class="nav_left">
+        <h2>nwave</h2>
+    </div>
+    <div class="nav_right">
+        <ul>
+            <li><a id="active" href="#">Home</a></li>
+            <li><a href="#">Over ons</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">Offerte</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+</div>
       <?php
 
       echo $this->section('content')
 
       ?>
+<div class="footer">
+
+</div>
 </body>
 </html>
