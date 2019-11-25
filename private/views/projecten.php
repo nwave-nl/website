4594dd635419ec82867bd2258296d3e14eb14b6e
YungYeno <43321063+YungYeno@users.noreply.github.com>
2019-11-25 19:55:43 +0100

    Update

--- private/views/projecten.php
+++ private/views/projecten.php
@@ -3,3 +3,22 @@
 <div class="intro">
     <h1>Projecten.</h1>
 </div>
+
+<div class="about__nwave--content">
+    <h1>Onze projecten</h1>
+</div>
+
+<div class="about__nwave--grid">
+
+    <div class="catering">
+        <img src="<?php echo image("projecten/starlounge_catering.jpg"); ?>" alt="Starlounge Catering">
+        <h3>Starlounge Catering</h3>
+    </div>
+
+
+<!--    <div>-->
+<!--        <img src="--><?php //echo image("projecten/starlounge_catering.jpg"); ?><!--" alt="Starlounge Catering">-->
+<!--        <h3>Starlounge Catering</h3>-->
+<!--    </div>-->
+
+</div>
