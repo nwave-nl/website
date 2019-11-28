95398ec87027185c56eeaedbf3f1858a3626b80e
YungYeno <43321063+YungYeno@users.noreply.github.com>
2019-11-28 10:24:39 +0100

    update

--- private/views/projecten.php
+++ private/views/projecten.php
@@ -4,24 +4,41 @@
     <h1>Projecten.</h1>
 </div>
 
+
+<div class=" text_projecten center">
+    <p>Hier kunt u de projecten bekijken welke wij hebben afgerond of waar wij mee bezig zijn. </p>
+</div>
+
+
 <div class="projecten__nwave--spacing"></div>
 
-<!--<div class="projecten_grid">-->
+<div class="projecten_grid">
 
-    <div class="catering">
-        <img src="<?php echo image("projecten/starlounge_catering.jpg"); ?>" alt="Starlounge Catering">
-        <h3>Starlounge Catering</h3>
+<!--Project StarLounge Catering-->
+    <div class="catering center">
+        <img src="<?php echo image("projecten/starlounge_catering.jpg"); ?>" alt="Starlounge Catering" />
+    </div>
+    <div class="uitleg_catering center">
+        <h1>Starlounge Catering</h1>
         <p>Coming soon...</p>
     </div>
+<!--Einde Project StarLounge Catering-->
 
     <div class="projecten__nwave--tekstspacing"></div>
 
-    <div class="imperium">
-        <img src="<?php echo image("projecten/imperium.jpg"); ?>" alt="Home Imperium">
-        <h3>Home Imperium</h3>
+
+<!--Project Home Imperium-->
+    <div class="imperium center">
+        <img src="<?php echo image("projecten/imperium.jpg"); ?>" alt="Home Imperium" />
+    </div>
+    <div class="uitleg_imperium center">
+        <h1>Home Imperium</h1>
         <p>Coming soon...</p>
     </div>
+<!--Einde Project Home Imperium-->
+</div>
 
 <div class="projecten__nwave--tekstspacing"></div>
 
-</div>
+
+
