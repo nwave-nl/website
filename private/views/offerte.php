3f5d42636fbb7ed81657fd5bebb272d02fd0aa3b
jelle <25534@ma-web.nl>
2019-12-01 21:46:30 +0100

    offerte update
    not done yet.

--- private/views/offerte.php
+++ private/views/offerte.php
@@ -4,3 +4,51 @@
 <div class="intro">
   <h1>Offerte aanvragen.</h1>
 </div>
+<div class="offerte__main">
+
+    <div class="offerte__main--spacing"></div>
+    <h1 class="offerte-h1">Informatie</h1>
+    <p class="offerte-p">Alle informatie over hoe wij te werk gaan met u.</p>
+    <div class="offerte__main--spacing"></div>
+
+    <div class="offerte__main--offerte--1">
+        <div class="offerte__main--middle">
+            <div class="offerte__main--content">
+                <h1>Offerte aanvragen.</h1>
+                <p>In de offerte zullen u vragen gesteld worden over de looks en functionaliteit van de website zoals u dat in gedachten heeft.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="offerte__main--offerte--2">
+        <div class="offerte__main--middle">
+            <div class="offerte__main--content">
+                <h1>Offerte bespreken.</h1>
+                <p>Het team van nwave zal zo spoedig mogelijk contact met
+                    opnemen om de offerte te bespreken. Tijdens deze bespreking zullen wij aantekeningen maken en een frisse blik geven op uw eigen ideeën.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="offerte__main--offerte--3">
+        <div class="offerte__main--middle">
+            <div class="offerte__main--content">
+                <h1>Aan de slag.</h1>
+                <p>Als de offerte is besproken zal het team aan de slag gaan met de
+                    wireframes en een functioneel ontwerp. Zo krijgt u een goede indruk van hoe de website eruit komt te zien.
+                    Als dat wordt goedgekeurd gaan we verder naar stap 4.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="offerte__main--offerte--4">
+        <div class="offerte__main--middle">
+            <div class="offerte__main--content">
+                <h1>Time to code.</h1>
+                <p>In deze fase gaan wij aan de slag met het realiseren van uw website. Het functionele ontwerp wordt aangehouden om verwarring
+                    te voorkomen en ervoor te zorgen dat u altijd tevreden bent.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
